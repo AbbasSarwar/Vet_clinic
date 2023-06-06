8b46b604e53d5f347da5ca8698b9f939dcec63f4
Abbas Sarwar <abbassarwar100@gmail.com>
2023-06-06 18:07:33 +0500

    Delete & Updated and Select filters

--- data.sql
+++ data.sql
@@ -2,3 +2,11 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (2, 'Gabumon', '2018-11-03', 2, B'1', 8);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (3, 'Pikachu', '2021-01-07', 1, B'0', 15.04);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (4, 'Devimon', '2017-05-23', 5, B'1', 11);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (5, 'Charmander', '2020-02-08', 0, B'0', 11);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (6, 'Plantmon', '2021-11-15', 2, B'1', -5.7);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (7, 'Squirtle', '1993-04-02', 3, B'0', -12.13);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (8, 'Angemon', '2015-06-12', 1, B'1', -45);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (9, 'Boarmon', '2005-06-07', 7, B'1', 20.4);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (10, 'Blossom', '1998-10-13', 3, B'1', 17);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (11, 'Ditto', '2022-05-14', 4, B'1', 22);
--- schema.sql
+++ schema.sql
@@ -2,6 +2,10 @@ CREATE TABLE animals(
 id int PRIMARY KEY NOT NULL,
 name varchar(40) NOT NULL,
 date_of_birth date NOT NULL,
+escape_attempts int NOT NULL,
 neutered bit,
-weight decimal
+weight_kg decimal
 )
+
+ALTER table animals 
+ADD COLUMN species varchar(50)
