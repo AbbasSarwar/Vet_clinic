1b1147c084400212cae06f6e42190934ab527134
Abbas Sarwar <abbassarwar100@gmail.com>
2023-06-07 21:17:09 +0500

    Multiple tables

--- data.sql
+++ data.sql
@@ -10,3 +10,60 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (9, 'Boarmon', '2005-06-07', 7, B'1', 20.4);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (10, 'Blossom', '1998-10-13', 3, B'1', 17);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES (11, 'Ditto', '2022-05-14', 4, B'1', 22);
+
+
+INSERT INTO owners (full_name, age) 
+VALUES 
+('Sam Smith', 34), 
+('Jennifer Orwell', 19), 
+('Bob', 45), 
+('Melody Pond', 77),
+('Dean Winchester', 14), 
+('Jodie Whittaker', 38)
+select * from owners
+
+INSERT INTO species (full_name) 
+VALUES 
+('POKEMON'), 
+('Digimon')
+select * from species
+
+
+UPDATE animals 
+SET species_id = 2
+WHERE name LIKE '%mon'
+
+select * from animals
+
+UPDATE animals
+SET species_id = 1
+WHERE species_id is NULL
+
+select * from animals
+
+
+UPDATE animals
+SET owner_id = (SELECT id from owners where full_name = 'Sam Smith')
+WHERE name = 'Agumon'
+select * from animals
+
+UPDATE animals
+SET owner_id = (SELECT id from owners where full_name = 'Jennifer Orwell')
+WHERE name in ('Gabumon', 'Pikachu')
+select * from animals
+
+UPDATE animals
+SET owner_id = (SELECT id from owners where full_name = 'Bob')
+WHERE name in ('Devimon', 'Plantmon')
+select * from animals
+
+UPDATE animals
+SET owner_id = (SELECT id from owners where full_name = 'Melody Pond')
+WHERE name in ('Charmander', 'Squirtle', 'Blossom')
+select * from animals
+
+UPDATE animals
+SET owner_id = (SELECT id from owners where full_name = 'Dean Winchester')
+WHERE name in ('Angemon', 'Boarmon')
+select * from animals
+
--- queries.sql
+++ queries.sql
@@ -88,3 +88,36 @@ max escape //by year
 SELECT species, AVG(escape_attempts) from animals
 WHERE date_of_birth > '1990-01-01' AND date_of_birth < '2000-12-31'
 GROUP BY species;
+
+
+//
+SELECT animals.name,owners.full_name from animals join owners
+on animals.owner_id = owners.id
+where full_name = 'Melody Pond'
+
+SELECT animals.name,species.full_name from animals join species
+on animals.species_id = species.id
+where full_name = 'POKEMON'
+
+SELECT animals.name,owners.full_name from animals LEFT join owners 
+on animals.owner_id = owners.id
+
+SELECT COUNT(*), species.full_name FROM animals JOIN species 
+on animals.species_id = species.id
+GROUP BY species.id;
+
+SELECT animals.name,species.full_name from animals 
+JOIN species ON species.id = animals.species_id
+JOIN owners ON owners.id = animals.owner_id
+WHERE species.full_name = 'Digimon'
+
+SELECT animals.name from animals JOIN owners 
+ON animals.species_id = owners.id
+WHERE animals.escape_attempts = 0 AND owners.full_name = 'Dean Winchester'
+
+SELECT owners.full_name, COUNT(animals.id) as total
+from animals JOIN owners 
+ON owners.id = animals.owner_id
+GROUP BY owners.full_name
+ORDER BY total DESC
+LIMIT 1
